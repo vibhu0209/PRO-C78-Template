13f0cf4ab29ccfa8f2a04fc1f725b39c94106b8f
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2021-09-28 19:33:46 +0530

    Update StarMap.js

--- screens/StarMap.js
+++ screens/StarMap.js
@@ -19,10 +19,8 @@ export default class StarMapScreen extends Component {
                 <SafeAreaView style={styles.droidSafeArea} />
                 <View style={{ flex: 0.3, marginTop: 20, alignItems: 'center' }}>
                 <Text 
-                // style={styles.titleText}
-                // style={{styles.titleText}}
-                // style=styles.titleText
-                // style={titleText}
+                style={styles.titleText}
+
                 >Star Map</Text>
 
                     <TextInput
@@ -30,29 +28,8 @@ export default class StarMapScreen extends Component {
                         placeholder="Enter your longitude"
                         placeholderTextColor="white"
 
-                        // onChangeText={(text) => {
-                        //     this.State({
-                        //         longitude: text
-                        //     })
-                        // }}
-
-                        // onChangeText={(text) => {
-                        //     this.setState(
-                        //         longitude: text
-                        //     )
-                        // }}
-
-                        // onChangeText={() => {
-                        //     this.setState({
-                        //         longitude: 
-                        //     })
-                        // }}
-
-                        // onChangeText={(text) => {
-                        //     this.setState({
-                        //         longitude: text
-                        //     })
-                        // }}
+                       .
+                        ................
                     />
 
                     <TextInput
